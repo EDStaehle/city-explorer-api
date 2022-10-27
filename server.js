37aa572a2e73339994bc1baad635c22433cad4a7
Elias <elias.staehle@gmail.com>
2022-10-26 20:54:56 -0400

    added third party api

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ console.log('our fist server');
 //******requires*****
 const express = require('express');
 require('dotenv').config();
-let data = require('./data/weather.json');
+// let data = require('./data/weather.json');
 const cors = require('cors');
+const axios = require('axios');
 // once express is in we need to use it
 // app === server
 const app = express();
@@ -21,15 +22,16 @@ const PORT = process.env.PORT || 3002;
 //******endpoints */
 // base endpoint
 
-app.get('/weather', (request, response,next) => {
+app.get('/weather', async(request, response,next) => {
   try {
     console.log('hit weather route');
-    let city = request.query.searchQuery;
-    let lat = Math.floor(request.query.lat);
-    let lon = Math.floor(request.query.lon);
-    let dataToGroom = data.find(cityObj => cityObj.city_name.toLowerCase() === city.toLowerCase() && Math.floor(cityObj.lat) === lat && Math.floor(cityObj.lon) === lon);
-    let dataToSend = dataToGroom.data.map(day => new Forcast(day));
-    console.log(dataToSend);
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+    let weatherResults = await axios.get(url);
+    console.log(weatherResults);
+    let dataToSend = weatherResults.data.data.map(day => new Forcast(day));
+    console.log('this is my data to send', dataToSend);
     response.status(200).send(dataToSend);
   } catch (error) {
     next(error);
@@ -43,7 +45,26 @@ class Forcast {
     this.key = cityObj._id;
   }
 }
+app.get('/movies', async(req, res, next) =>{
+  try {
+    let cityName = req.query.city_name;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${cityName}&language=en-US&page=1&include_adult=false`;
+    let movieResults = await axios.get(url);
+    let dataToSend = movieResults.data.results.map(mov => new Movies(mov));
+    console.log(dataToSend);
+    res.status(200).send(dataToSend);
+  } catch (error) {
+    next(error);
+  }
+});
 
+class Movies {
+  constructor(movObj){
+    this.title = movObj.title;
+    this.overview = movObj.overview;
+    this.img = movObj.poster_path;
+  }
+}
 app.get('*', (request, response) => {
   response.status(404).send('this route does not exist');
 });
